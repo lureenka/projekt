f193ec97ad594547b4e09df6dd1d05e55f30d7f2
lureenka <kaosinska1996@gmail.com>
2024-06-17 10:42:05 +0200

    Create post_sign_up.py

---
+++ post_sign_up.py
@@ -0,0 +1,9 @@
+from api.data.register import RegisterRequestDto
+from api.base_api import BaseAPI
+
+class SignUp(BaseAPI):
+    def api_call(self, user: RegisterRequestDto):
+        user_data = user.to_dict()
+        headers = {'Content-Type': 'application/json'}
+        response = self.make_request("POST", "users/signup", json=user_data, headers=headers)
+        return response
